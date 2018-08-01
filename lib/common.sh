86da7a8aeb78857d44b0725aece9c6760aa18f53
Henry Stanley <henry@henrystanley.com>
2018-08-01 17:31:04 -0400

    create graal tempdir

--- lib/common.sh
+++ lib/common.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
 install_graalvm() {
+  mkdir -p /tmp/graalvm
+
   GRAALVM_DOWNLOAD_URL=${GRAALVM_DOWNLOAD_URL:-"https://github.com/oracle/graal/releases/download/vm-1.0.0-rc4/graalvm-ce-1.0.0-rc4-linux-amd64.tar.gz"}
   curl --retry 3 --location $GRAALVM_DOWNLOAD_URL | tar xzm -C /tmp/graalvm 
 
