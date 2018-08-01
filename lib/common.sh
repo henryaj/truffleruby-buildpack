7d45bbfb77aa4bd70129e995fdfe3884087b1b0c
Henry Stanley <henry@henrystanley.com>
2018-08-01 17:51:00 -0400

    remove wrapping directory from extracted graal

--- lib/common.sh
+++ lib/common.sh
@@ -4,7 +4,7 @@ install_graalvm() {
   mkdir -p /tmp/graalvm
 
   GRAALVM_DOWNLOAD_URL=${GRAALVM_DOWNLOAD_URL:-"https://github.com/oracle/graal/releases/download/vm-1.0.0-rc4/graalvm-ce-1.0.0-rc4-linux-amd64.tar.gz"}
-  curl --retry 3 --location $GRAALVM_DOWNLOAD_URL | tar xzm -C /tmp/graalvm 
+  curl --retry 3 --location $GRAALVM_DOWNLOAD_URL | tar xzm -C /tmp/graalvm --strip-components 1
 
   export PATH=/tmp/graalvm/bin:$PATH
 }
